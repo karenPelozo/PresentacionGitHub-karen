33a1d4a17f5e68147cd27cecdd079d9c122600e2
karenpelozo <karenpelozo98@gmail.com>
2023-03-20 20:22:21 -0300

    agruge un hola mundo en un localHost

--- src/app.js
+++ src/app.js
@@ -1,1 +1,8 @@
-console.log("hola mundo")
+const express =  require('express'); //llamamos express de node_modules para usarla 
+const app = express();
+const PORT = 3001;
+app.get('/api', (req, res) =>{
+    res.json({mensaje : 'hola mundo' }).status(200);//formato para intercambiar archivos .status manda todo ok 
+})//el get recibe 2 parametros string , funcion de flechita la fucion dos parametros req 
+//NECECITA ESTAR ECUCHANDO 
+app.listen(PORT,()=>{console.log('estoy escuchando en el puerto 3001')});//.listen recibe 2 parametros un entero y una funcion
